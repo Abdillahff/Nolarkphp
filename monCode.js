6ea06369f5ee917603f5e20ba74352813ba74dfd
Abdillahff <127401809+Abdillahff@users.noreply.github.com>
2023-04-06 10:39:42 +0200

    Add files via upload

---
+++ monCode.js
@@ -0,0 +1,6 @@
+
+window.addEventListener("onload", function() {
+    window.document.querySelector("") .addEventListener("onload", function() {
+        alert("coucou");
+    }, false);
+}, false);
